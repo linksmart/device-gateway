71bf2dd21aee5bd0abf95cf6965489ba45a31068
Farshid Tavakolizadeh <email@farshid.ws>
2018-01-05 13:51:18 +0100

    added version to logs

--- main.go
+++ main.go
@@ -15,10 +15,12 @@ import (
 
 var confPath = flag.String("conf", "conf/device-gateway.json", "Device gateway configuration file path")
 var BuildNumber = "N/A"
+var Version = "N/A"
 
 func main() {
 	fmt.Print(LINKSMART)
 	logger.Printf("Starting Device Gateway")
+	logger.Printf("Version: %s", Version)
 	logger.Printf("Build Number: %s", BuildNumber)
 	flag.Parse()
 
